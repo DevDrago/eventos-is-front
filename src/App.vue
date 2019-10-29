ea0e7a8a7fcf26ed0eee4733f0eed6cd0b0a0e5a
Saul Lainez <saullainez@hotmail.es>
2019-10-29 15:31:23 -0600

    Proteger rutas

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 				<mdb-navbar-nav right>
 					<mdb-nav-item href="#">Usuarios</mdb-nav-item>
 					<mdb-nav-item href="#">Eventos</mdb-nav-item>
+					<mdb-nav-item @click="cerrarSesion" v-if="isLoggedIn">Cerrar sesión</mdb-nav-item>
 				</mdb-navbar-nav>
 			</mdb-navbar-toggler>
 		</mdb-navbar>
@@ -36,6 +37,8 @@
 
 <script>
 import { mdbFooter, mdbContainer, mdbRow, mdbCol, mdbDropdown, mdbDropdownToggle, mdbDropdownMenu, mdbDropdownItem, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem } from 'mdbvue';
+import {mapActions, mapGetters} from 'vuex';
+import router from './router/index';
 export default {
 	name: "App",
 	components: {
@@ -52,6 +55,15 @@ export default {
     mdbDropdownToggle,
     mdbDropdownMenu,
     mdbDropdownItem
+	},
+	methods: {
+		...mapActions(['logout']),
+		cerrarSesion(){
+			this.logout().then(()=>router.push({name:'LoginRegister'}));
+		}
+	},
+	computed: {
+		...mapGetters(['isLoggedIn'])
 	}
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,48 @@
+/* eslint-disable callback-return */
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '../store';
 import LoginRegister from '@/components/LoginRegister';
 import Dashboard from '@/components/Dashboard';
+import Profile from '@/components/Profile';
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'LoginRegister',
+      component: LoginRegister,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
+    }
+  ],
+  linkActiveClass: "active",
+  mode: "history"
+});
+
+export default router;
+
+/*export default new router({
   routes: [
     {
       path: '/',
@@ -15,9 +52,48 @@ export default new Router({
     {
       path: '/admin',
       name: 'Admin',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
     }
   ],
   linkActiveClass: "active",
   mode: "history"
+});*/
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      if (to.matched.some(record => record.meta.is_admin)){
+        if (store.getters.isAdmin){
+          // eslint-disable-next-line callback-return
+          next();
+        } else {
+          next('/profile');
+        }
+      } else {
+        next();
+        return;
+      }
+    }
+    // eslint-disable-next-line callback-return
+    next('/'); 
+  } else if (to.matched.some(record => record.meta.guest)){
+    if (store.getters.isLoggedIn) {
+      if (store.getters.isAdmin){
+        next('/admin');
+      } else {
+        next('/profile');
+      }
+    }
+    else {
+      next();
+    }
+  } 
+  else {
+    // eslint-disable-next-line callback-return
+    next(); 
+  }
 });
