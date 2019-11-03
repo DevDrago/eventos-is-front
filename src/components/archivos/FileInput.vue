4b907d7bdb13b067af43372892bc1fd1d6006f21
mauriciogalo <omgalo@unah.hn>
2019-11-02 21:29:09 -0600

    FileInput migrasado typescript

--- src/components/archivos/FileInput.vue
+++ src/components/archivos/FileInput.vue
@@ -3,74 +3,78 @@
     Everything is wrapped in a label, which acts as a clickable wrapper around a form element.
     In this case, the file input.
   -->
-  <div 
-    style=""
-  >
+  <div style>
     <v-layout align-center wrap justify-space-between row fill-height>
       <v-flex xs12>
-        <v-btn
-          block
-          color="#64b5f6"
-          class="white--text mx-0"
-          @click="abrir"
-        >
+        <v-btn block color="#64b5f6" class="white--text mx-0" @click="abrir">
           Cargar Imagen
-          <v-icon right dark>photo</v-icon>
+          <v-icon right dark>mdi-image</v-icon>
         </v-btn>
       </v-flex>
       <v-flex xs12 md9 v-if="name">
-        <v-text-field
-          readonly
-          v-model="archivo.name"
-          label="Archivo seleccionado"
-        ></v-text-field>
+        <v-text-field readonly v-model="archivo.name" label="Archivo seleccionado"></v-text-field>
       </v-flex>
       <v-flex xs12 pt-0>
         <slot />
       </v-flex>
     </v-layout>
-  <!-- Now, the file input that we hide. -->
-  <input ref="archivo" type="file" @change="handleFileChange" :multiple="multiple" :accept="accept"/>
-
+    <!-- Now, the file input that we hide. -->
+    <input
+      ref="archivoInput"
+      type="file"
+      @change="handleFileChange"
+      :multiple="multiple"
+      :accept="accept"
+    />
   </div>
 </template>
-<script>
-export default {
-  name: 'file-input',
-  data(){
-    return {
-      archivo: [],
-    }
-  },
-  props: {
-    // Using value here allows us to be v-model compatible.
-    value: '',
-    multiple: false,
-    accept: '',
-    name: false
-  },
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 
-  methods: {
-    handleFileChange(e) {
-      // Whenever the file changes, emit the 'input' event with the file data.
-      //this.$set(this.archivo,'archivos',e.target.files)
-      this.archivo = [];
-      for (const archivo of e.target.files) {
+interface FileInputEventTarget extends EventTarget {
+  files: FileList;
+}
+
+interface FileInputEvent extends Event {
+  srcElement: FileInputEventTarget;
+}
+
+@Component({
+  name: "file-input"
+})
+export default class Login extends Vue {
+  @Prop() private value!: any;
+  @Prop() private multiple!: boolean;
+  @Prop() private accept!: String;
+  @Prop() private name!: boolean;
+
+  $refs!: {
+    archivoInput: HTMLElement;
+  };
+
+  private archivo: Array<File> = [];
+
+  private handleFileChange(e: FileInputEvent) {
+    let inputFile: FileInputEventTarget = e.srcElement;
+    this.archivo = [];
+
+    if (inputFile !== null) {
+      for (const archivo of inputFile.files) {
         console.log(archivo);
-        this.archivo.push(archivo)
+        this.archivo.push(archivo);
       }
-      this.$emit('input', this.archivo)
-    },
-    abrir() {
-      this.$refs.archivo.click();
     }
+    this.$emit("input", this.archivo);
+  }
+
+  private abrir() {
+    this.$refs.archivoInput.click();
   }
 }
 </script>
+
 <style scoped>
-/* Don't forget to hide the original file input! */
 input[type="file"] {
   display: none;
 }
-
 </style>
