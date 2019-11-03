eb7937994d29ed8a8c887e43215e57dbfea5096e
mauriciogalo <omgalo@unah.hn>
2019-11-03 16:06:40 -0600

    Añadido icono de avatar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-app id="inspire">
-      <v-navigation-drawer v-model="drawer" app clipped >
+      <v-navigation-drawer v-model="drawer" app clipped>
         <v-list dense>
           <v-list-item :to="{ name: 'home'}" exact>
             <v-list-item-action>
@@ -19,7 +19,7 @@
               <v-list-item-title>Dashboard</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-          <v-list-item  :to="{ name: 'actividades'}">
+          <v-list-item :to="{ name: 'actividades'}">
             <v-list-item-action>
               <v-icon>mdi-settings</v-icon>
             </v-list-item-action>
@@ -32,22 +32,46 @@
 
       <v-app-bar app clipped-left dense color="light-blue darken-3" dark>
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title >{{$appName}}</v-toolbar-title>
+        <v-toolbar-title>{{$appName}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-menu offset-y class="mx-5">
+          <template v-slot:activator="{ on }">
+            <v-btn dark icon v-on="on">
+              <v-avatar size="32px">
+                <!-- <img v-if="usuario.imgPerfil" :src="usuario.imgPerfil" alt="Vuetify" /> -->
+                <img
+                  src="https://cdn3.iconfinder.com/data/icons/users-groups/512/female-512.png"
+                  alt="Vuetify"
+                />
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list class="px-5">
+            <v-list-item>
+              <v-btn icon to="/perfil" style="color: #232323 !important">
+                <v-icon>mdi-account</v-icon>Perfil
+              </v-btn>
+            </v-list-item>
+            <v-list-item>
+              <v-btn icon>
+                <v-icon>mdi-logout</v-icon>Salir
+              </v-btn>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-app-bar>
 
       <v-content>
         <v-container fluid fill-height>
           <v-row align="center" justify="center">
-            <router-view>
-                
-            </router-view>
+            <router-view></router-view>
           </v-row>
         </v-container>
       </v-content>
 
       <!-- <v-footer app color="light-blue darken-3" dark>
         <span>&copy; 2019</span>
-      </v-footer> -->
+      </v-footer>-->
     </v-app>
   </div>
 </template>
@@ -56,7 +80,7 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class Home extends Vue {
-  private drawer: null;
+  private drawer: boolean = false;
   constructor() {
     super();
   }
